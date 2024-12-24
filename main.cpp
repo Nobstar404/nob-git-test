e8f5db2af80a04aa1e348f6d9442d77f5f09e340
Nobstar404 <iqbalfadhillah973@gmail.com>
2024-12-24 18:14:08 +0700

    menambahkan void log

--- main.cpp
+++ main.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 
+void log(const char* mesage);
+
 int main () {
     std::cout << "hello world\n";
 
+    log("hello world");
+
     return 0;
 }
+void log(const char* mesage);
